77693f0789967e726eface54fb19f62888487071
SoumilAggarwal <aggarwal.soumil15@gmail.com>
2024-03-12 14:56:22 +0530

    adding user profile form

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -1,5 +1,10 @@
 import { CircleUserRound } from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { Link } from "react-router-dom";
@@ -7,30 +12,40 @@ import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 
 const UsernameMenu = () => {
-    const {user, logout}=useAuth0();
-  return(
+  const { user, logout } = useAuth0();
+  return (
     <DropdownMenu>
-        <DropdownMenuTrigger className="flex items-center ps-3 font-bold hover:text-orange-500 gap-2">
-            <CircleUserRound className="text-orange-500" />
-            {user?.email}
-        </DropdownMenuTrigger>
-        <DropdownMenuContent >
-            <DropdownMenuItem>
-                <Link to="/user-profile" className="font-bold hover:text-orange-500">
-                    User Profile
-                </Link>
-            </DropdownMenuItem>
-            <Separator></Separator>
-            <DropdownMenuItem>
-                <Button className="flex flex-1 font-bold bg-orange-500" 
-                    onClick={()=>logout()}
-                >
-                    Logout
-                </Button>
-            </DropdownMenuItem>
-        </DropdownMenuContent>
+      <DropdownMenuTrigger className="flex items-center ps-3 font-bold hover:text-orange-500 gap-2">
+        <CircleUserRound className="text-orange-500" />
+        {user?.email}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <Link
+            to="/manage-restaurant"
+            className="font-bold hover:text-orange-500"
+          >
+            Manage Restaurant
+          </Link>
+        </DropdownMenuItem>
+        <Separator></Separator>
+        <DropdownMenuItem>
+          <Link to="/user-profile" className="font-bold hover:text-orange-500">
+            User Profile
+          </Link>
+        </DropdownMenuItem>
+        <Separator></Separator>
+        <DropdownMenuItem>
+          <Button
+            className="flex flex-1 font-bold bg-orange-500"
+            onClick={() => logout()}
+          >
+            Logout
+          </Button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
 
 export default UsernameMenu;
